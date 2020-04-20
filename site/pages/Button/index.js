f6ea4abf1edee9611694f54a7f372fd62d04ee78
z-9527 <137596665@qq.com>
2020-04-20 11:32:56 +0800

    新增路径重命名

--- site/pages/Button/index.js
+++ site/pages/Button/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Button from '../../../components/button';
+import Button from '@components/button';
 
 class ButtonPage extends Component {
   state = {}
